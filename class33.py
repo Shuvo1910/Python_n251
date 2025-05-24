3b76469d9e2a491c1d19e74a5c71fd2caca70af6
Shuvo1910 <sultanshuvo1910@gmail.com>
2025-05-24 22:56:41 +0600

    class 33 updated

--- class33.py
+++ class33.py
@@ -1,4 +1,22 @@
 print ("Hello"+"World")
+# # Timer:
+
+import time
+import sys
+
+my_timer = int(input("Enter time in seconds: "))
+
+
+for remaining in range(my_timer, -1, -1):
+    h = remaining // 3600
+    m = (remaining % 3600) // 60
+    s = remaining % 60
+   
+    sys.stdout.write(f"\r{h:02d}:{m:02d}:{s:02d}")
+    sys.stdout.flush()
+    time.sleep(1)
+
+print("\nTime's up!")
 
 
 
